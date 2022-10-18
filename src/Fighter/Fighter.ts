84a3cd6f973f08328244399db75e898f71ffaa8b
GiovannaMorais <gigimorais9@hotmail.com>
2022-10-18 16:49:37 -0300

    Feat: Fighter.ts - code refactoring

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,12 +1,11 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
 
-export default interface Fighter {
+export default interface Fighter extends SimpleFighter {
   lifePoints: number;
   strength: number;
   defense: number;
   energy?: Energy;
-  attack(enemy:Fighter):void;
   special?(enemy:Fighter):void;
   levelUp():void;
-  receiveDamage(attackPoints:number):void;
 }
