79e041f68aa477dd20bc3c90eaa441977a177155
GiovannaMorais <gigimorais9@hotmail.com>
2022-10-18 16:05:11 -0300

    Feat: Class Necromancer

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Necromancer extends Archetype {
+  _energyType: EnergyType;
+  static _instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this._instances += 1;
+    return this._instances;
+  }
+}
